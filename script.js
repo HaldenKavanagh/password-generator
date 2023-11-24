d27432be6dfad430665f0878f24af961e1c5a235
Halden Kavanagh <haldenkav@gmail.com>
2023-11-24 15:41:59 -0700

    added random generator for lowercase only and length selector

--- script.js
+++ script.js
@@ -3,6 +3,21 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var character = prompt(
+    "how many characters would you like your password to be? (between 8 and 120)"
+  );
+  let password = "";
+  let str = "abcdefghijklmnopqrstuvwxyx";
+
+  for (let i = 0; i < character; i++) {
+    let char = Math.floor(Math.random() * str.length + 1);
+
+    password += str.charAt(char);
+  }
+
+  return password;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,7 +29,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-  alert("test");
-}
