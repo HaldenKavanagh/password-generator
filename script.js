ff046b8b5229d3a5fa53bc4a133fee68314d26c9
Halden Kavanagh <haldenkav@gmail.com>
2023-11-25 14:45:17 -0700

    js work

--- script.js
+++ script.js
@@ -4,13 +4,35 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var character = prompt(
-    "how many characters would you like your password to be? (between 8 and 120)"
+  // criteria prompts
+
+  var length = prompt(
+    "How many characters would you like your password to be? (between 8 and 120)"
   );
+  var lowercaseConfirm = confirm("Do you want to use lowercase letters?");
+  var uppercaseConfirm = confirm(
+    "Would you like to include uppercase letters?"
+  );
+  var numbersConfirm = confirm("Would you like to include numbers?");
+  var charactersConfirm = confirm(
+    "Would you like to include special charactors?"
+  );
+
+  // password variables
+
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var characters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  // calls for correct criteria
+
+  // final password generator
+
   let password = "";
   let str = "abcdefghijklmnopqrstuvwxyx";
 
-  for (let i = 0; i < character; i++) {
+  for (let i = 0; i < length; i++) {
     let char = Math.floor(Math.random() * str.length + 1);
 
     password += str.charAt(char);
